a1e22b6a7104fa14ec1b43b0776cac0c9d7cc9e5
TMusilova <tereza154@post.cz>
2025-10-20 19:10:59 +0200

    init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DorucovaciSluzba.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
