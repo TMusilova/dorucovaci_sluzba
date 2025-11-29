220228ca0b88c7e80cc7d64af11ad157f3549118
Mikydosh <michal.dolik@email.cz>
2025-11-29 14:23:28 +0100

    Registrační a přihlašovací formulář

---
+++ DorucovaciSluzba/Areas/Security/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DorucovaciSluzba.Areas.Security.Controllers
+{
+    public class AccountController : Controller
+    {
+        [Area("Security")]
+        public class AccountsController : Controller 
+        {
+            public IActionResult Register()
+            {
+                return View();
+            }
+
+            public IActionResult Login()
+            {
+                return View();
+            }
+        }
+    }
+}
